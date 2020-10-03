88d33f7db24c0c4a63b9466d9c0289a3f3f1fbe3
NETXCCODE <72317380+NETXCCODE@users.noreply.github.com>
2020-10-04 02:03:17 +0700

    Update kerja.py

--- userbot/modules/kerja.py
+++ userbot/modules/kerja.py
@@ -157,7 +157,12 @@ async def typewriter(typew):
 	message = typew.pattern_match.group(0)
 	sleep(0)
 	await typew.edit("`Untuk melihat tampilan yang di inginkan\n Silahkan cek`\n [Disini](https://t.me/DemoScript) \n#DemoWeb")
-
+	
+@register(outgoing=True, pattern='^.ucok(?: |$)(.*)')
+async def typewriter(typew):
+	message = typew.pattern_match.group(0)
+	sleep(0)
+	await typew.edit("okeeee")
 
 # Create by myself @JejakCheat
 
